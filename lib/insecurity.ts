b69bb007ff8adfa5f838cb2a60968a4aea15c2e4
Livqonn <88245375+KaygusuzAbdal@users.noreply.github.com>
2023-10-22 02:38:45 +0300

    Update insecurity.ts

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -40,7 +40,8 @@ interface IAuthenticatedUsers {
   updateFrom: (req: Request, user: ResponseWithUser) => any
 }
 
-export const hash = (data: string) => crypto.createHash('md5').update(data).digest('hex')
+const bcrypt = require("bcrypt")
+export const hash = (data: string) => bcrypt.hashSync(password, salt).digest('hex')
 export const hmac = (data: string) => crypto.createHmac('sha256', 'pa4qacea4VK9t9nGv7yZtwmj').update(data).digest('hex')
 
 export const cutOffPoisonNullByte = (str: string) => {
